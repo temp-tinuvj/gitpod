130601d0466d599c05ffc58a772818a36832a113
Gero Posmyk-Leinemann <gero@gitpod.io>
2021-03-18 10:31:10 +0000

    [db, protocol, server] Basic WorkspaceCluster support

---
+++ components/gitpod-db/src/typeorm/entity/db-workspace-cluster.ts
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2020 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
+import { PrimaryColumn, Column, Entity, Index } from "typeorm";
+import { WorkspaceCluster, WorkspaceClusterState } from "@gitpod/gitpod-protocol/lib/workspace-cluster";
+
+@Entity()
+export class DBWorkspaceCluster implements WorkspaceCluster {
+    @PrimaryColumn()
+    name: string;
+
+    @Column({
+        type: "varchar",
+        length: 255,
+    })
+    url: string;
+
+    @Column({
+        type: "blob",
+        nullable: true,
+        transformer: {
+            to(value: any): any {
+                if (!value) {
+                    // map ["", null, undefined] -> null
+                    return null;
+                }
+                return value;
+            },
+            from(value: any): any {
+                if (!value) {
+                    // map ["", null, undefined] -> undefined
+                    return undefined;
+                }
+                if (Buffer.isBuffer(value) && (value as Buffer).length === 0) {
+                    // TypeORM seems to map MySQL 'NULL' to an empty buffer. We translate to 'undefined' here.
+                    return undefined;
+                }
+                return value;
+            }
+        }
+    })
+    certificate?: Buffer;
+
+    @Index("ind_state")
+    @Column({
+        type: "char",
+        length: 20,
+    })
+    state: WorkspaceClusterState;
+
+    @Column()
+    score: number;
+
+    @Column()
+    maxScore: number;
+
+    @Index("ind_controller")
+    @Column({
+        type: "char",
+        length: 20,
+    })
+    controller: string;
+}
